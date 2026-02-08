0e0ceced58cf4fa275c386f4510632660eb6ddae
AmirRezaDavarzani <am.davarzani@storex.local>
2026-02-08 13:58:00 +0330

    first commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { sync } from "glob";
+
+export default {
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: sync("./src/**/*.html".replace(/\\/g, "/")),
+    },
+    // output: {
+    //   entryFileNames: "assets/js/[name].[hash].js", // فایل‌های JS در assets/js
+    //   chunkFileNames: "assets/js/[name].[hash].js", // فایل‌های Chunk در assets/js
+    //   assetFileNames: ({ name }) => {
+    //     if (name && name.endsWith(".html")) {
+    //       return "src/views/[name].[hash][extname]"; // فایل‌های HTML در src/views
+    //     } else if (name && name.endsWith(".css")) {
+    //       return "assets/css/[name].[hash][extname]"; // فایل‌های CSS در assets/css
+    //     } else if (name && /\.(png|jpe?g|svg|gif)$/.test(name)) {
+    //       return "assets/images/[name].[hash][extname]"; // تصاویر در assets/images
+    //     }
+    //     return "assets/[name].[hash][extname]"; // سایر دارایی‌ها
+    //   },
+    // },
+  },
+};
